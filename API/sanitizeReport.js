ce1f9be2198233a835f524f7adc6f0ff9b3d190f
PierreSwtich <skamper28@gmail.com>
2024-08-02 11:53:28 +0200

    fix(api): add g flag to sanitize token in all instances

--- API/sanitizeReport.js
+++ API/sanitizeReport.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+// This script ensures that every instance of "Authorization" is sanitized, regardless of its position in the file.
+
 const reportPath = path.join(__dirname, 'results', 'index.html');
 // const reportPath = path.join(__dirname, 'index.html');
 
@@ -14,7 +16,8 @@ fs.readFile(reportPath, 'utf8', (err, data) => {
   let sanitizedData = data.replace(/"username":\s*"[^"]*"/g, '"username": "REDACTED"')
                           .replace(/"password":\s*"[^"]*"/g, '"password": "REDACTED"')
                           .replace(/"token":\s*"[^"]*"/g, '"token": "REDACTED"')
-                          .replace(/"Authorization":\s*"Bearer [^"]*"/g, '"Authorization": "Bearer REDACTED"');
+                          .replace(/"Authorization":\s*"Bearer [^"]*"/g, '"Authorization": "Bearer REDACTED"')
+                          .replace(/"Authorization":\s*"[^"]*"/g, '"Authorization": "REDACTED"');
   
   fs.writeFile(reportPath, sanitizedData, 'utf8', (err) => {
     if (err) {
